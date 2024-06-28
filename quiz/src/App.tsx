bac0d604ce53e37da771b02e187aeaf386103f8b
luka chikashvili <lukachikashvili2020@gmail.com>
2024-06-28 17:11:23 +0400

    add background and border

---
+++ quiz/src/App.tsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="app">
+          <div className="border">
+             
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default App
